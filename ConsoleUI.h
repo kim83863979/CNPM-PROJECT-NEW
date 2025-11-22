5f8f49955359caec001121b5a721f84ef20aa0a7
kim83863979 <kimlq2957@ut.edu.vn>
2025-11-23 01:39:45 +0700

    Refactor ConsoleUI.h to use #pragma once

--- ConsoleUI.h
+++ ConsoleUI.h
@@ -1,41 +1,18 @@
-// ConsoleUI.h
-#ifndef CONSOLEUI_H
-#define CONSOLEUI_H
-
+#pragma once
 #include <string>
-using namespace std;
 
 class ConsoleUI {
 public:
-    // Hiển thị màn hình đăng nhập
     static void displayLoginScreen();
-
-    // Nhận thông tin đăng nhập
-    static void getLoginCredentials(string& username, string& password);
-
-    // Hiển thị menu sinh viên
-    static void displayStudentMenu(const string& studentName, const string& studentID);
-
-    // Nhận lựa chọn của sinh viên
+    static void getLoginCredentials(std::string& username, std::string& password);
+    static void displayStudentMenu(const std::string& studentName, const std::string& studentID);
     static int getStudentChoice();
-
-    // Hiển thị menu admin
-    static void displayAdminMenu(const string& adminName);
-
-    // Nhận lựa chọn của admin
+    static void displayAdminMenu(const std::string& adminName);
     static int getAdminChoice();
-
-    // Hiển thị thông báo
-    static void showMessage(const string& message);
-
-    // Nhận input từ người dùng
-    static string promptInput(const string& promptMessage);
-
-    // Xóa màn hình (tùy hệ điều hành)
+    static void showMessage(const std::string& message);
+    static std::string promptInput(const std::string& promptMessage);
     static void clearScreen();
-
-    // Dừng màn hình chờ nhấn Enter
     static void pressEnterToContinue();
 };
 
-#endif
+
